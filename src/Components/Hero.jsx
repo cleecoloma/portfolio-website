ad71860355645df2a10454973414ba0c03732f2d
Chester Lee Coloma <cleecoloma@gmail.com>
2023-11-12 22:26:10 -0800

    added projects and contact me buttons

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from '@mui/material/Button';
 import './Hero.css';
 
 function Hero() {
@@ -10,6 +11,16 @@ function Hero() {
       </h1>
       <h3>Full-stack Software Developer</h3>
       <hr />
+      <p id='hero-message'>
+        I build <span id='hero-message-span'>digital bridges</span> that connect{' '}
+        <strong>ideas</strong> with the <strong>world.</strong>
+      </p>
+      <Button id='project-button' className='button-style' variant='contained'>
+        See my projects
+      </Button>
+      <Button id='contact-button' className='button-style' variant='outlined'>
+        Contact me
+      </Button>
     </>
   );
 }
