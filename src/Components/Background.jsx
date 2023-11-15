273a7d898d35bfd3f81e7dde0721e51d7f0ae105
Chester Lee Coloma <cleecoloma@gmail.com>
2023-11-14 21:34:14 -0800

    added background title and description

--- src/Components/Background.jsx
+++ src/Components/Background.jsx
@@ -4,8 +4,23 @@ import './Background.css';
 
 function Background() {
   return (
-    <div>Background</div>
-  )
+    <div className='background-container'>
+      <h4>BACKGROUND</h4>
+      <p>
+        Former <strong>mechanical engineer</strong> with an active secret
+        clearance and <strong>8+ years of professional working experience</strong> in team
+        oriented roles. Delivered technically sound solutions to restore US Navy
+        submarines back to 100% operational condition. <br />
+        <br /> I see myself as a <strong>results driven</strong> individual
+        where I understand the importance of delivering solutions that meet the
+        requirements and expectations. <br />
+        <br />
+        <strong>Fun fact:</strong> When I'm not coding, you'll find me diving
+        into the world of lawn care - determined to have the best lawn in the
+        neighborhood. It's my little green oasis project beyond the keyboard.
+      </p>
+    </div>
+  );
 }
 
 export default Background
