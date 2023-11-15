4c27e3be123ccdfcfe91c4a466fc367f45692bf2
Chester Lee Coloma <cleecoloma@gmail.com>
2023-11-14 22:00:06 -0800

    styled and added icons

--- src/Components/Background.jsx
+++ src/Components/Background.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import MenuIcon from '@mui/icons-material/Menu';
+import SettingsIcon from '@mui/icons-material/Settings';
+import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
+import CodeIcon from '@mui/icons-material/Code';
 import './Background.css';
 
 function Background() {
   return (
     <div className='background-container'>
       <h4>BACKGROUND</h4>
-      <p>
+      <p id='background-description'>
         Former <strong>mechanical engineer</strong> with an active secret
-        clearance and <strong>8+ years of professional working experience</strong> in team
+        clearance and{' '}
+        <strong>8+ years of professional working experience</strong> in team
         oriented roles. Delivered technically sound solutions to restore US Navy
         submarines back to 100% operational condition. <br />
         <br /> I see myself as a <strong>results driven</strong> individual
@@ -19,8 +23,13 @@ function Background() {
         into the world of lawn care - determined to have the best lawn in the
         neighborhood. It's my little green oasis project beyond the keyboard.
       </p>
+      <div id='icon-container'>
+        <SettingsIcon id='gear-icon' />
+        <ArrowRightAltIcon id='arrow-icon' />
+        <CodeIcon id='code-icon' />
+      </div>
     </div>
   );
 }
 
-export default Background
+export default Background;
