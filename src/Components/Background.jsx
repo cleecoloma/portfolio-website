a0d386e7e9fe62950ae91730595f4bddf80c5d10
Chester Lee Coloma <cleecoloma@gmail.com>
2023-11-26 22:32:32 -0800

    added initial what i do file structure

--- src/Components/Background.jsx
+++ src/Components/Background.jsx
@@ -1,35 +1,16 @@
 import React from 'react';
-import MenuIcon from '@mui/icons-material/Menu';
-import SettingsIcon from '@mui/icons-material/Settings';
-import ArrowRightAltIcon from '@mui/icons-material/ArrowRightAlt';
-import CodeIcon from '@mui/icons-material/Code';
 import './Background.css';
 
-function Background() {
+function WhatIDo() {
   return (
-    <div className='background-container'>
-      <h4>BACKGROUND</h4>
-      <p id='background-description'>
-        Former <strong>mechanical engineer</strong> with an active secret
-        clearance and{' '}
-        <strong>8+ years of professional working experience</strong> in team
-        oriented roles. Delivered technically sound solutions to restore US Navy
-        submarines back to 100% operational condition. <br />
-        <br /> I see myself as a <strong>results driven</strong> individual
-        where I understand the importance of delivering solutions that meet the
-        requirements and expectations. <br />
-        <br />
-        <strong>Fun fact:</strong> When I'm not coding, you'll find me diving
-        into the world of lawn care - determined to have the best lawn in the
-        neighborhood. It's my little green oasis project beyond the keyboard.
+    <div className='what-i-do-container'>
+      <h4>WHAT I DO</h4>
+      <p id='what-i-do-description'>
       </p>
-      <div id='icon-container'>
-        <SettingsIcon id='gear-icon' />
-        <ArrowRightAltIcon id='arrow-icon' />
-        <CodeIcon id='code-icon' />
+      <div id='tool-container'>
       </div>
     </div>
   );
 }
 
-export default Background;
+export default WhatIDo;
