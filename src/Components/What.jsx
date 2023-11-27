e00a641d8ca7bcf1a9e9c4e5e3fdeddb19b73b3d
Chester Lee Coloma <cleecoloma@gmail.com>
2023-11-26 22:48:29 -0800

    added react-bootstrap to utilize Grid

--- src/Components/What.jsx
+++ src/Components/What.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import './What.css';
 
 function What() {
@@ -9,6 +10,16 @@ function What() {
         I’m a <strong>full-stack software developer</strong> with a focus on
         responsive design and user interface (UI) development.
       </p>
+      <Container>
+        <Row>
+          <Col>1</Col>
+          <Col>2</Col>
+        </Row>
+        <Row>
+          <Col>3</Col>
+          <Col>4</Col>
+        </Row>
+      </Container>
       <div id='tool-container'></div>
     </div>
   );
