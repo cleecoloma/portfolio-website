bb1b6e87a34ea1fe82f0d2fe057be8d291233b88
Chester Lee Coloma <cleecoloma@gmail.com>
2023-11-29 12:01:11 -0800

    added form and its styling

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,19 +1,33 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import './Contact.css';
 
 function Contact() {
   return (
     <div className='contact-container'>
       <h4>CONTACT</h4>
-      <p id='contact-description'>
-        Together, let's bring ideas to life.
-      </p>
-      <Button>Send Message</Button>
-      <div id='link-container'>
+      <p id='contact-description'>Together, let's bring ideas to life.</p>
+      <div className='form-container'>
+        <Form>
+          <Form.Group className='mb-3' controlId='exampleForm.ControlInput1'>
+            <Form.Label>Name</Form.Label>
+            <Form.Control type='text' placeholder='name' />
+          </Form.Group>
+          <Form.Group className='mb-3' controlId='exampleForm.ControlInput1'>
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type='email' placeholder='name@example.com' />
+          </Form.Group>
+          <Form.Group className='mb-3' controlId='exampleForm.ControlTextarea1'>
+            <Form.Label>Message</Form.Label>
+            <Form.Control as='textarea' rows={5} />
+          </Form.Group>
+        </Form>
+        <Button>Send Message</Button>
       </div>
+      <div id='link-container'></div>
     </div>
   );
 }
 
-export default Contact
+export default Contact;
