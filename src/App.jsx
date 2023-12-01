d576573200c6fd161a01544a80574573ff293439
Chester Lee Coloma <cleecoloma@gmail.com>
2023-11-30 23:13:01 -0800

    updated projects desktop css names as its affecting other screen sizes

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
     return () => {
       window.removeEventListener('resize', handleResize);
     };
-  }, []);
+  });
   
   return (
     <>
--- src/Components/ProjectsDesktop.jsx
+++ src/Components/ProjectsDesktop.jsx
@@ -4,28 +4,25 @@ import './ProjectsDesktop.css';
 
 function ProjectsDesktop() {
   return (
-    <div className='projects-container'>
+    <div className='projects-desktop-container'>
       <h4>FEATURED PROJECTS</h4>
-      <div className='projects-card'>
-        <img
-          className='card-img'
-          src='../../public/images/job-application-tracker.png'
-        />
-        <div className='card-right'>
-          <div className='card-header'>
+      <div className='projects-desktop-card'>
+        <img src='../../public/images/job-application-tracker.png' />
+        <div className='card-desktop-right'>
+          <div className='card-desktop-header'>
             <p id='card-title'>Job Application Tracker</p>
           </div>
-          <div className='card-links'>
+          <div className='card-desktop-links'>
             <Button variant='primary'>DEMO</Button>
             <Button variant='primary'>CODE</Button>
           </div>
-          <div id='card-text'>
+          <div id='card-desktop-text'>
             A mobile-responsive job application tracker web application using
             the MERN (MongoDB, Express, React, Node) stack, enabling users to
             seamlessly create, manage, and track job listings across four
             categories (Interested, Applied, Interview, Rejected).
           </div>
-          <div className='card-skills'>
+          <div className='card-desktop-skills'>
             <Button variant='primary'>TOOLS</Button>
             <Button variant='primary'>TOOLS</Button>
             <Button variant='primary'>TOOLS</Button>
@@ -33,26 +30,23 @@ function ProjectsDesktop() {
           </div>
         </div>
       </div>
-      <div className='projects-card'>
-        <img
-          className='card-img'
-          src='../../public/images/job-application-tracker.png'
-        />
-        <div className='card-right'>
-          <div className='card-header'>
+      <div className='projects-desktop-card'>
+        <img src='../../public/images/job-application-tracker.png' />
+        <div className='card-desktop-right'>
+          <div className='card-desktop-header'>
             <p id='card-title'>Job Application Tracker</p>
           </div>
-          <div className='card-links'>
+          <div className='card-desktop-links'>
             <Button variant='primary'>DEMO</Button>
             <Button variant='primary'>CODE</Button>
           </div>
-          <div id='card-text'>
+          <div id='card-desktop-text'>
             A mobile-responsive job application tracker web application using
             the MERN (MongoDB, Express, React, Node) stack, enabling users to
             seamlessly create, manage, and track job listings across four
             categories (Interested, Applied, Interview, Rejected).
           </div>
-          <div className='card-skills'>
+          <div className='card-desktop-skills'>
             <Button variant='primary'>TOOLS</Button>
             <Button variant='primary'>TOOLS</Button>
             <Button variant='primary'>TOOLS</Button>
@@ -60,26 +54,23 @@ function ProjectsDesktop() {
           </div>
         </div>
       </div>
-      <div className='projects-card'>
-        <img
-          className='card-img'
-          src='../../public/images/job-application-tracker.png'
-        />
-        <div className='card-right'>
-          <div className='card-header'>
+      <div className='projects-desktop-card'>
+        <img src='../../public/images/job-application-tracker.png' />
+        <div className='card-desktop-right'>
+          <div className='card-desktop-header'>
             <p id='card-title'>Job Application Tracker</p>
           </div>
-          <div className='card-links'>
+          <div className='card-desktop-links'>
             <Button variant='primary'>DEMO</Button>
             <Button variant='primary'>CODE</Button>
           </div>
-          <div id='card-text'>
+          <div id='card-desktop-text'>
             A mobile-responsive job application tracker web application using
             the MERN (MongoDB, Express, React, Node) stack, enabling users to
             seamlessly create, manage, and track job listings across four
             categories (Interested, Applied, Interview, Rejected).
           </div>
-          <div className='card-skills'>
+          <div className='card-desktop-skills'>
             <Button variant='primary'>TOOLS</Button>
             <Button variant='primary'>TOOLS</Button>
             <Button variant='primary'>TOOLS</Button>
@@ -87,7 +78,7 @@ function ProjectsDesktop() {
           </div>
         </div>
       </div>
-      <Button variant='success' id='see-more-button'>
+      <Button variant='success' id='desktop-see-more-button'>
         SEE MORE PROJECTS
       </Button>
     </div>
