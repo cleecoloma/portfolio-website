750e9fa1465e55c2adbe061d7ebee80e083f708a
Chester Lee Coloma <cleecoloma@gmail.com>
2023-11-30 19:53:43 -0800

    updated hero section for desktop screens

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -6,27 +6,35 @@ import './Hero.css';
 function Hero() {
   return (
     <div className='hero-container'>
-      <img src='../../images/chester.jpeg' alt='chester' id='hero-img' />
-      <h1>
-        <span>CHESTER LEE</span> COLOMA
-      </h1>
-      <h3>Full-stack Software Developer</h3>
-      <hr />
-      <p id='hero-message'>
-        I build <span id='hero-message-span'>digital bridges</span> that connect{' '}
-        <strong>ideas</strong> with the <strong>world.</strong>
-      </p>
-      <div id='button-container'>
-        <Button
-          id='project-button'
-          className='button-style'
-          variant='contained'
-        >
-          See my projects
-        </Button>
-        <Button id='contact-button' className='button-style' variant='outlined'>
-          Contact me
-        </Button>
+      <div className='left-div'>
+        <img src='../../images/chester.jpeg' alt='chester' id='hero-img' />
+        <h1>
+          <span>CHESTER LEE</span> COLOMA
+        </h1>
+        <h3>Full-stack Software Developer</h3>
+        <hr />
+      </div>
+      <div className='right-div'>
+        <p id='hero-message'>
+          I build <span id='hero-message-span'>digital bridges</span> that
+          connect <strong>ideas</strong> with the <strong>world.</strong>
+        </p>
+        <div id='button-container'>
+          <Button
+            id='project-button'
+            className='button-style'
+            variant='contained'
+          >
+            See my projects
+          </Button>
+          <Button
+            id='contact-button'
+            className='button-style'
+            variant='outlined'
+          >
+            Contact me
+          </Button>
+        </div>
       </div>
       <div id='menu-icon-div'>
         <MenuIcon id='menu-icon' />
