fd23c79e4058b451165fd7fdf655b13479b2d50e
Chester Lee Coloma <cleecoloma@gmail.com>
2023-12-05 02:44:43 -0800

    updated styling for all components to fit mid screen sizes

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
 
   useEffect(() => {
     const handleResize = () => {
-      const isDesktopQuery = window.matchMedia('(min-width: 835px)').matches;
+      const isDesktopQuery = window.matchMedia('(min-width: 834px)').matches;
       setIsDesktop(isDesktopQuery);
 
       const showHeaderQuery = window.matchMedia('(min-width: 425px)').matches;
