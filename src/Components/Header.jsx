4b20370eb60d447102361adb588a56528c9d1470
Chester Lee Coloma <cleecoloma@gmail.com>
2023-12-05 00:28:00 -0800

    updated header code and styling:

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,3 +1,5 @@
+'use strict';
+
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
@@ -6,14 +8,25 @@ import '../styles/Header.css'
 
 function Header() {
   return (
-    <Navbar id='header-navbar'>
+    <Navbar id='header-navbar' fixed='top'>
       <Container>
-        <Navbar.Brand href='#home'>CC</Navbar.Brand>
-        <Nav className='me-auto'>
-          <Nav.Link href='#background'>Background</Nav.Link>
-          <Nav.Link href='#skills'>Skills</Nav.Link>
-          <Nav.Link href='#projects'>Projects</Nav.Link>
-          <Nav.Link href='#contact'>Contact</Nav.Link>
+        <Navbar.Brand id='header-brand'>CC</Navbar.Brand>
+        <Nav className='me-auto' variant='underline'>
+          <Nav.Link href='#home' className='header-links'>
+            Home
+          </Nav.Link>
+          <Nav.Link href='#background' className='header-links'>
+            Background
+          </Nav.Link>
+          <Nav.Link href='#skills' className='header-links'>
+            Skills
+          </Nav.Link>
+          <Nav.Link href='#projects' className='header-links'>
+            Projects
+          </Nav.Link>
+          <Nav.Link href='#contact' className='header-links'>
+            Contact
+          </Nav.Link>
         </Nav>
       </Container>
     </Navbar>
