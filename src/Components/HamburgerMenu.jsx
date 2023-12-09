f55bd1fc7a4956fea252c95d17ee69fbc981d441
Chester Lee Coloma <cleecoloma@gmail.com>
2023-12-08 18:24:53 -0800

    added hamburger menu styling

--- src/Components/HamburgerMenu.jsx
+++ src/Components/HamburgerMenu.jsx
@@ -1,43 +1,48 @@
 'use strict';
 
-import React from 'react';
+import React, { useState } from 'react';
+import CloseIcon from '@mui/icons-material/Close';
 import MenuIcon from '@mui/icons-material/Menu';
-import { Modal, Button } from 'react-bootstrap';
+import Button from '@mui/material/Button';
+import Nav from 'react-bootstrap/Nav';
 import '../styles/HamburgerMenu.css';
 
 function HamburgerMenu() {
-    const [showModal, setShowModal] = useState(false);
+  const [showMenu, setShowMenu] = useState(false);
 
-    const handleModalOpen = () => {
-      setShowModal(true);
-    };
+  const handleMenuToggle = () => {
+    setShowMenu(!showMenu);
+  };
 
-    const handleModalClose = () => {
-      setShowModal(false);
-    };
   return (
-    <>
-      <div id='hamburger-menu' onClick={handleModalOpen}>
-        <MenuIcon />
-      </div>
-      <Modal show={showModal} onHide={handleModalClose} centered>
-        <Modal.Header closeButton>
-          <Modal.Title>Menu</Modal.Title>
-        </Modal.Header>
-        {/* Add your menu content inside Modal.Body */}
-        <Modal.Body>
-          <p>Menu Item 1</p>
-          <p>Menu Item 2</p>
-          {/* Add more menu items as needed */}
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant='secondary' onClick={handleModalClose}>
-            Close
-          </Button>
-          {/* Add additional buttons or actions if needed */}
-        </Modal.Footer>
-      </Modal>
-    </>
+    <div id='hamburger-menu' onClick={handleMenuToggle}>
+      {showMenu && (
+        <div className='expanded-menu'>
+          <Nav
+            id='mobile-navigation-links'
+            className='me-auto'
+            variant='underline'
+          >
+            <Nav.Link href='#home' className='header-links'>
+              Home
+            </Nav.Link>
+            <Nav.Link href='#background' className='header-links'>
+              Background
+            </Nav.Link>
+            <Nav.Link href='#skills' className='header-links'>
+              Skills
+            </Nav.Link>
+            <Nav.Link href='#projects' className='header-links'>
+              Projects
+            </Nav.Link>
+            <Nav.Link href='#contact' className='header-links'>
+              Contact
+            </Nav.Link>
+          </Nav>
+        </div>
+      )}
+      {showMenu ? <CloseIcon id='close-button' /> : <MenuIcon />}
+    </div>
   );
 }
 
