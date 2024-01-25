76369e54dfc330122a3546949dba966b47953497
Chester Lee Coloma <cleecoloma@gmail.com>
2024-01-25 15:16:18 -0800

    updated light mode icon color during dark mode

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -11,6 +11,8 @@ import '../styles/Header.css';
 function Header() {
   const { isMode, toggleMode } = useContext(ThemeContext);
 
+  const linkClass = isMode ? null : 'header-link-dark';
+
   return (
     <Navbar
       className={isMode ? 'header-navbar-light' : 'header-navbar-dark'}
@@ -18,31 +20,33 @@ function Header() {
     >
       <Navbar.Brand id='header-brand'>
         <span id='first-c'>C</span>
-        <span id='second-c'>C</span>
+        <span id={isMode ? 'second-c-light' : 'second-c-dark'}>C</span>
       </Navbar.Brand>
       <Nav id='navigation-links' variant='underline'>
         <div id='nav-left'>
-          <Nav.Link href='#home' className='header-links'>
+          <Nav.Link href='#home' className={`header-links ${linkClass}`}>
             Home
           </Nav.Link>
-          <Nav.Link href='#background' className='header-links'>
+          <Nav.Link href='#background' className={`header-links ${linkClass}`}>
             Background
           </Nav.Link>
-          <Nav.Link href='#skills' className='header-links'>
+          <Nav.Link href='#skills' className={`header-links ${linkClass}`}>
             Skills
           </Nav.Link>
-          <Nav.Link href='#projects' className='header-links'>
+          <Nav.Link href='#projects' className={`header-links ${linkClass}`}>
             Projects
           </Nav.Link>
-          <Nav.Link href='#contact' className='header-links'>
+          <Nav.Link href='#contact' className={`header-links ${linkClass}`}>
             Contact
           </Nav.Link>
         </div>
-        <div id='nav-right' className='ml-auto' onClick={toggleMode}>
+        <div id={isMode ? 'nav-right' : 'nav-right-dark'}className='ml-auto' onClick={toggleMode}>
           {isMode ? (
             <DarkModeIcon fontSize='large' />
           ) : (
-            <LightModeIcon fontSize='large' />
+            <LightModeIcon
+              fontSize='large'
+            />
           )}
         </div>
       </Nav>
