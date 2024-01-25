ceb4084068385bcf0af1beba1e9ef4eb302ac34a
Chester Lee Coloma <cleecoloma@gmail.com>
2024-01-25 12:37:56 -0800

    added light and dark mode icons

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,18 +1,24 @@
 'use strict';
 
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import '../styles/Header.css'
+import LightModeIcon from '@mui/icons-material/LightMode';
+import DarkModeIcon from '@mui/icons-material/DarkMode';
+import '../styles/Header.css';
 
 function Header() {
+
+  const [isMode, setMode] = useState(false);
+
   return (
     <Navbar id='header-navbar' fixed='top'>
-        <Navbar.Brand id='header-brand'>
-          <span id='first-c'>C</span>
-          <span id='second-c'>C</span>
-        </Navbar.Brand>
-        <Nav id='navigation-links' className='me-auto' variant='underline'>
+      <Navbar.Brand id='header-brand'>
+        <span id='first-c'>C</span>
+        <span id='second-c'>C</span>
+      </Navbar.Brand>
+      <Nav id='navigation-links' className='me-auto' variant='underline'>
+        <div id='nav-left'>
           <Nav.Link href='#home' className='header-links'>
             Home
           </Nav.Link>
@@ -28,9 +34,13 @@ function Header() {
           <Nav.Link href='#contact' className='header-links'>
             Contact
           </Nav.Link>
-        </Nav>
+        </div>
+        <div id='nav-right'>
+          { isMode ? <LightModeIcon /> : <DarkModeIcon />}
+        </div>
+      </Nav>
     </Navbar>
   );
 }
 
-export default Header
+export default Header;
