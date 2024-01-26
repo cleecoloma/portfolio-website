39f27c395f588f69a947625e90e0836f339ed4a4
Chester Lee Coloma <cleecoloma@gmail.com>
2024-01-26 15:12:26 -0800

    added skills filter for dark mode

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,20 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import BuildIcon from '@mui/icons-material/Build';
+import { ThemeContext } from '../context/Theme';
 import '../styles/Skills.css';
 
 function Skills({ id }) {
+  const { isMode } = useContext(ThemeContext);
+
   return (
     <div className='skills-container' id={id}>
       <h4>SKILLS</h4>
       <div className='skills'>
-        <img className='skills-img' src='../../images/languages.gif' />
-        <img className='skills-img' src='../../images/react-hooks.gif' />
+        <img className={isMode ? 'skills-img' : 'skills-img skills-img-dark'} src='../../images/languages.gif' />
+        <img className={isMode ? 'skills-img' : 'skills-img skills-img-dark'} src='../../images/react-hooks.gif' />
         <img
-          className='skills-img'
+          className={isMode ? 'skills-img' : 'skills-img skills-img-dark'}
           src='../../images/frameworks-libraries.gif'
         />
-        <img className='skills-img' src='../../images/cloud-services.gif' />
-        <img className='skills-img' src='../../images/tools-technologies.gif' />
+        <img className={isMode ? 'skills-img' : 'skills-img skills-img-dark'} src='../../images/cloud-services.gif' />
+        <img className={isMode ? 'skills-img' : 'skills-img skills-img-dark'} src='../../images/tools-technologies.gif' />
       </div>
       <div id='icon-container'>
         <BuildIcon id='build-icon' />
